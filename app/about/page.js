6e9a2eb981d9e0089d1a453a39a79643f086626d
pvv-star <politov.vasile@gmail.com>
2026-02-26 12:38:12 +0200

    revert: remove Next.js files

--- app/about/page.js
+++
@@ -1,9 +0,0 @@
-import AboutContent from '@/components/AboutContent';
-
-export const metadata = {
-  title: 'Despre | Adevăr.md',
-};
-
-export default function AboutPage() {
-  return <AboutContent />;
-}
